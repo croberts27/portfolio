096fb01aa609849940548f51f46b15841d731b27
Calvin Roberts <you@example.com>
2023-12-31 13:30:58 -0700

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./index.css";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Footer from "./components/Footer";
