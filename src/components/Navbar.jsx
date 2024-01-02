1fbc52d898bdaa945493fc04f3521ba40aa17bbf
Calvin Roberts <you@example.com>
2024-01-02 14:15:26 -0700

    navbar mobile friendly

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { Disclosure } from "@headlessui/react";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -7,14 +9,76 @@ function classNames(...classes) {
 
 function Navbar() {
   const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false); // State to manage dropdown visibility
 
   return (
-    <header className="absolute  shadow-md inset-x-0 top-0 z-50">
+    <header className="absolute shadow-md inset-x-0 top-0 z-50">
       <nav
-        className="flex items-center justify-between
-   p-6 lg:px-12"
+        className="flex flex-col lg:flex-row items-start justify-between p-5 lg:px-12"
         aria-label="Global"
       >
+        {/* Mobile Menu */}
+        <Disclosure as="div" className="lg:hidden">
+          <Disclosure.Button
+            className="inline-flex justify-start items-start border-gray-300 text-xl font-medium focus:outline-none"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            Menu
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-7"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
+              />
+            </svg>
+          </Disclosure.Button>
+          <Disclosure.Panel
+            className={classNames(
+              "bg-white shadow-md absolute top-full left-0 right-0 z-50",
+              { hidden: !isOpen }
+            )}
+          >
+            <div className="flex flex-col gap-4 p-4">
+              <Link
+                to="/"
+                className="text-black text-xl font-medium"
+                onClick={() => setIsOpen(false)}
+              >
+                About
+              </Link>
+              <Link
+                to="/blog"
+                className="text-black text-xl font-medium"
+                onClick={() => setIsOpen(false)}
+              >
+                Blogs
+              </Link>
+              <Link
+                to="/projects"
+                className="text-black text-xl font-medium"
+                onClick={() => setIsOpen(false)}
+              >
+                Projects
+              </Link>
+              <Link
+                to="/skills"
+                className="text-black text-xl font-medium"
+                onClick={() => setIsOpen(false)}
+              >
+                Skills
+              </Link>
+            </div>
+          </Disclosure.Panel>
+        </Disclosure>
+
+        {/* Desktop Menu */}
         <div className="hidden lg:flex lg:gap-x-12">
           <Link
             className={`border-gray-300 inline-flex items-center border-b-2 px-1 pt-1 text-xl font-medium`}
@@ -28,7 +92,6 @@ function Navbar() {
           >
             Blogs
           </Link>
-
           <Link
             className={`border-gray-300 inline-flex w-full items-center justify-center border-b-2 px-1 pt-1 text-xl font-medium text-black `}
             to="/projects"
@@ -40,15 +103,6 @@ function Navbar() {
             to="/skills"
           >
             Skills
-          </Link>
-        </div>
-        <div className="flex lg:flex-1 justify-end items-center">
-          <Link className="-m-1.5 p-1.5" to="/">
-            <img
-              className="h-20 w-auto rounded-full"
-              src="/profPic.jpg"
-              alt="Calvin Matson-Roberts"
-            />
           </Link>
         </div>
       </nav>
--- src/data.jsx
+++ src/data.jsx
@@ -1,18 +1,12 @@
 /* eslint-disable no-useless-escape */
-import BlogImage from "/medium.jpg";
-import LegendSmithImg from "/legend-smith.jpg";
-import JATEImg from "/jate.jpg";
-import RecyculatorImg from "/recyculator-main.jpg";
-import PasswordGenImg from "/psswrd-generator.jpg";
-import UntitledImg from "/untitled.jpg";
-import TCIImg from "/tci-logo.jpg";
+
 export const projects = [
   {
     title: "Legend Smith",
     subtitle: "Forge Legends with LegendSmith: Where Characters Come to Life!",
     description:
       "LegendSmith uses your choices and top-notch AI to generate amazing characters. Whether you're a gamer looking for a character to sink your teeth into, or a writer searching for a great story prompt, LegendSmith is here to kick-start your imagination.",
-    image: { LegendSmithImg },
+    image: "/legend-smith.jpg",
     link: "https://github.com/branchwag/character-generator",
   },
   {
@@ -20,7 +14,7 @@ export const projects = [
     subtitle: "Text Editor",
     description:
       "Downloadable text editor application for general note taking.",
-    image: { JATEImg },
+    image: "/jate.jpg",
     link: "https://github.com/croberts27/text-editor?tab=readme-ov-file",
   },
   {
@@ -28,7 +22,7 @@ export const projects = [
     subtitle: "Your Guide to Eco-Friendly Recycling Solutions",
     description:
       "Recyculator is a website that provides information to users on how to dispose of waste items in the most environmentally sustainable way, while providing them the location of their nearest recycling center.",
-    image: { RecyculatorImg },
+    image: "/recyculator-main.jpg",
     link: "https://croberts27.github.io/recyculator/",
   },
   {
@@ -36,7 +30,7 @@ export const projects = [
     subtitle: "Random Password Generator",
     description:
       "This app can be used to randomly generate a secure password for any use!",
-    image: { PasswordGenImg },
+    image: "/psswrd-generator.jpg",
     link: "https://croberts27.github.io/password-generator-hw-3/",
   },
   {
@@ -44,7 +38,7 @@ export const projects = [
     subtitle: "An social e-commerce online art gallery",
     description:
       "A platform for artists to find inspiration and share their work",
-    image: { UntitledImg },
+    image: "/untitled.jpg",
     link: "https://github.com/vlexaega/project3-untitled",
   },
   {
@@ -52,7 +46,7 @@ export const projects = [
     subtitle: "Where Hospitality Meets Sporting Ambitions.",
     description:
       "Webpage for TCI Housing so clients can find information on sporting events and contact for assistance in booking housing for their event.",
-    image: { TCIImg },
+    image: "/tci-logo.jpg",
     link: "https://tcihousing.com",
   },
 ];
@@ -76,19 +70,19 @@ export const blogs = [
   {
     title: "Utilizing Backend Code to Render Data on the Frontend",
     subtitle: "Medium Article",
-    image: { BlogImage },
+    image: "/medium.jpg",
     link: "https://medium.com/@calvinroberts1998/utilizing-backend-code-to-render-data-on-the-frontend-3ddf5f9aa861",
   },
   {
     title: "Implementing PayPal in React App",
     subtitle: "Medium Article",
-    image: { BlogImage },
+    image: "/medium.jpg",
     link: "https://medium.com/@calvinroberts1998/implementing-paypal-in-react-app-5c27e885777b",
   },
   {
     title: "Learning A New Technology: Server-side APIs",
     subtitle: "Medium Article",
-    image: { BlogImage },
+    image: "/medium.jpg",
     link: "https://medium.com/@calvinroberts1998/learning-a-new-technology-server-side-apis-e685a9e630c6",
   },
 ];
